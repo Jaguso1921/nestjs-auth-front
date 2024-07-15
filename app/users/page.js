fc2f14d94d736db61b0c8446a3eb6de17b4d81a6
Jaguso1921 <115105445+Jaguso1921@users.noreply.github.com>
2024-07-15 17:04:25 -0400

    Actualizacion page

--- app/users/page.js
+++ app/users/page.js
@@ -1,78 +1,113 @@
-"use client"
-import React, {useEffect, useState} from 'react';
-import {Container, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { Button, Container, Grid, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import IconButton from '@mui/material/IconButton';
-import {Edit} from "@mui/icons-material";
+import { Edit } from "@mui/icons-material";
+import dayjs from 'dayjs';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 import AuthService from "../../services/AuthService";
-import { useRouter } from 'next/navigation';
 import Navbar from '../../components/Navbar';
+import Filters from '@/components/Filters';
 
-export default function Users(){
-
+export default function Users() {
     const router = useRouter();
     const [users, setUsers] = useState([]);
+    const [tokenExpired, setTokenExpired] = useState(false);
+    const [name, setName] = useState();
+    const [active, setActive] = useState();
+    const [login_after_date, setLogin_after_date] = useState();
+    const [login_before_date, setLogin_before_date] = useState();
 
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'));
-        if(!user){
+        if (!user) {
             router.push('/login');
-        }
-        if(user?.roles?.includes('admin')){
-            getAllUsers();
-        }
-        if(user?.roles?.includes('user')){
-            getUser(user.id);
         }
     }, []);
 
-    const getAllUsers = async () => {
-        const data = await AuthService.getUsers();
-        setUsers(data);
-    }
-
-    const getUser = async (id) => {
+    useEffect(() => {
         const token = localStorage.getItem('token');
-        const data = await AuthService.getUserById(id, token);
-        setUsers([data]);
-    }
+        (async () => {
+            const data = await AuthService.getUsers({
+                name: name === '' ? undefined : name,
+                active: active,
+                login_after_date: login_after_date && dayjs(login_after_date).format('YYYY-MM-DD'),
+                login_before_date: login_before_date && dayjs(login_before_date).format('YYYY-MM-DD')
+            }, token);
 
-    const handleEdit = (user) => {
-        router.push('/users/' + user.id + '/edit');
+            console.log(data);
+            if (!data || data?.message === 'Expired Token') {
+                setTokenExpired(true);
+            }
+            setUsers(data);
+        })();
+    }, [name, active, login_after_date, login_before_date]);
+
+    if (tokenExpired) {
+        return (
+            <div>
+                <h2>Token expirado</h2>
+                <Link href="/login">Inicie sesión otra vez</Link>
+            </div>
+        );
     }
 
     return (
         <Container>
-            <Navbar />
-            <h1>Users</h1>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Nombre</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Estado</TableCell>
-                        <TableCell>Última Sesión</TableCell>
-                        <TableCell>Acciones</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        users.map((user) => (
-                            <TableRow key={user}>
-                                <TableCell>{user.name}</TableCell>
-                                <TableCell>{user.email}</TableCell>
-                                <TableCell>{user.status? 'ACTIVO' : 'CERRADO'}</TableCell>
-                                <TableCell>TBD</TableCell>
-                                <TableCell>
-                                    <IconButton color="primary" aria-label={"Editar usuario " + user.name} onClick={() => handleEdit(user)}>
-                                        <Edit />
-                                    </IconButton>
-                                </TableCell>
+            {users === null ? (
+                <h1>Cargando...</h1>
+            ) : (
+                <>
+                    <Navbar />
+                    <Grid container spacing={4} padding={10}>
+                        <Grid xs={2} padding={2}>
+                            <h1>Usuarios</h1>
+                        </Grid>
+                        <Grid xs={3} padding={2}>
+                            <Link href="/users/create">
+                                <Button color='primary' variant='contained'>Añadir usuarios -&gt;</Button>
+                            </Link>
+                        </Grid>
+                    </Grid>
+                    <Filters
+                        active={active}
+                        onChange={setActive}
+                        name={name}
+                        onNameChange={setName}
+                        loginAfterDate={login_after_date}
+                        loginBeforeDate={login_before_date}
+                        onLoginAfterDateChange={setLogin_after_date}
+                        onLoginBeforeDateChange={setLogin_before_date}
+                    />
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Nombre</TableCell>
+                                <TableCell>Email</TableCell>
+                                <TableCell>Estado</TableCell>
+                                <TableCell>Última Sesión</TableCell>
+                                <TableCell>Acciones</TableCell>
                             </TableRow>
-                        ))
-                    }
-                </TableBody>
-            </Table>
+                        </TableHead>
+                        <TableBody>
+                            {users.map((user) => (
+                                <TableRow key={user.id}>
+                                    <TableCell>{user.name}</TableCell>
+                                    <TableCell>{user.email}</TableCell>
+                                    <TableCell>{user.status ? 'ACTIVO' : 'CERRADO'}</TableCell>
+                                    <TableCell>{user.Sessions[0]?.createdAt ? dayjs(user.Sessions[0]?.createdAt).format('YYYY-MM-DD HH:mm:ss') : 'Nunca'}</TableCell>
+                                    <TableCell>
+                                        <IconButton color="primary" aria-label={`Editar usuario ${user.name}`} onClick={() => handleEdit(user)}>
+                                            <Edit />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </>
+            )}
         </Container>
-    )
+    );
 }
